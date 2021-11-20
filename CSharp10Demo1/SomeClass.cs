24bc6f7f522500ada538a31777733fc8a8df739d
Jasper Kent <software@jasperkent.com>
2021-11-20 11:00:04 +0000

    Add project files.

---
+++ CSharp10Demo1/SomeClass.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp10Demo1;
+
+internal class SomeClass
+{
+    public SomeClass()
+    {
+        Console.WriteLine();
+
+        using var writer = new StringWriter();
+
+        writer.WriteLine("...");
+
+        var builder = new StringBuilder();
+
+        var x = Cos(PI / 2);
+
+    }
+}
